969542e92ca224bb506ffbcd3c63d0a637becd7d
merithes <illuofhabbo@yahoo.fr>
2017-08-02 21:59:03 +0200

    heh

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vboivin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 20:10:38 by vboivin           #+#    #+#             */
+/*   Updated: 2017/08/02 21:53:36 by vboivin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "hls.h"
 
 void				lengths_solo(long int *lengths, char **tabs, char *options)
@@ -33,12 +45,13 @@ int					chk_d(char **tab, int i, char *opt, int *type)
 
 	if (!(dir_id = opendir(tab[i])))
 	{
-		if (lstat(tab[i], &statf))
+		if (lstat(tab[i], &statf) && ++type[3])
 			(!type[0]) ? ft_putstr_cat(NEXIST_1, tab[i], NEXIST_2, 1) : 1;
-		else if (statf.st_mode >= S_IFREG && statf.st_mode < S_IFLNK)
-			(type[0] && type[0] != 2 && (type[1] = 2)) ?
-				print_solo_file(tab, i, opt, statf) : 1;
-		else if (!type[0])
+		else if (statf.st_mode >= S_IFREG &&
+			statf.st_mode < S_IFLNK && ++type[3])
+				(type[0] && type[0] != 2 && (type[1] = 2)) ?
+					print_solo_file(tab, i, opt, statf) : 1;
+		else if (!type[0] && ++type[3])
 			ft_putstr_cat(NPERMS_1, tab[i], NPERMS_2, 1);
 		return (0);
 	}
@@ -52,25 +65,27 @@ int					main(int ac, char **av)
 {
 	char			*opt;
 	int				i;
-	int				wit[3];
+	int				wit[5];
 	int				is_inp_a_file;
 
-	ft_bzero(wit, 3 * sizeof(int));
+	ft_bzero(wit, 4 * sizeof(int));
 	opt = NULL;
 	is_inp_a_file = 0;
 	opt = get_opt(ac, av);
 	while (!(i = 0) && wit[0] <= 1)
 	{
 		while (av[++i])
-			(av[i][0] != '-') ? wit[2] += (chk_d(av, i, opt, wit) == 0) 1 : 180; 
-		(wit[0]++ && (wit[2] % 180)) ? ft_putstr("\n") : 1;
+			if (av[i][0] != '-')
+				wit[2] += (chk_d(av, i, opt, wit) != 0) ? 1 : 0;
+		(wit[0]++ && wit[3]) ? ft_putstr("\n\n") : 1;
 	}
+	wit[4] = (wit[3] && wit[2]) ? 2 : 0;
+	wit[2] = (wit[2] / 2 > 1) ? 2 : 0;
 	while (++i < ac)
 	{
-		printf("%d\t%d\t%d\n", wit[0], wit[1], wit[2]);
 		if (av[i][0] != '-' && chk_d(av, i, opt, wit))
-			list_dir(opt, av[i], NULL, 0 + wit[1] + wit[2]);
-		(wit[2] / 2) ? ft_putchar('\n') : 1;
+			list_dir(opt, av[i], NULL, 0 + wit[1] + wit[2] + wit[4]);
+		(wit[2] / 2 && i + 1 < ac) ? ft_putchar('\n') : 1;
 	}
 	opt ? free(opt) : ac++;
 	return 0;
--- truc.c
+++ truc.c
@@ -1,11 +1,57 @@
-#include <stdio.h>
-#include <sys/stat.h>
-#include <pwd.h>
-#include <time.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   truc.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vboivin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 21:45:48 by vboivin           #+#    #+#             */
+/*   Updated: 2017/08/02 21:53:57 by vboivin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return 0;
+}
+
+void	print_nbrs(int *nbr)
+{
+	ft_putchar(nbr[0] + '0');
+	ft_putchar(nbr[1] + '0');
+	ft_putchar(nbr[2] + '0');
+	if (nbr[0] != 7)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
 
 int		main(int ac, char **av)
 {
-	int i = 0;
+	int nbr[3];
 
-	printf("%d", S_IFREG);
+	nbr[0] = 0;
+	nbr[1] = 0;
+	nbr[2] = 0;
+
+	while (nbr[0] < 8)
+	{
+		nbr[1] = nbr[0] + 1;
+		while (nbr[1] < 9)
+		{
+			nbr[2] = nbr[1] + 1;
+			while (nbr[2] <= 9)
+			{
+				if (nbr[0] < nbr[1] && nbr[1] < nbr[2])
+					print_nbrs(nbr);
+				nbr[2]++;
+			}
+			nbr[1]++;
+		}
+		nbr[0]++;
+	}
 }
